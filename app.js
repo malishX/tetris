282db348c9ef0e155b093323efb56eefeff8136f
Muhammad Ali <hschool510@gmail.com>
2020-06-13 00:44:42 +0400

    Tetris javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const width = 10
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtm = document.querySelector('#start-button')
+
+})
